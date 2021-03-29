30316289fcfedcc78a8ddd71943535f4f6691a05
pilillo <>
2021-03-29 22:53:47 +0200

    first commit

---
+++ conf/config.js
@@ -0,0 +1,8 @@
+var config = {
+    "catalogue" : {
+        "endpoint" : "http://localhost:8085/asset/"
+    },
+    "featurestore" : {
+        "endpoint" : "http://localhost:8085/featureset/"
+    }
+};
